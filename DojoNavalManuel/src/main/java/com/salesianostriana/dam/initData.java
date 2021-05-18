5c34bc19d9861827de5f8eb1b88cb9832845c2bc
Manuel Naval <navalrod@gmail.com>
2021-05-18 21:08:25 +0200

    Formulario de imagenes funcional

--- DojoNavalManuel/src/main/java/com/salesianostriana/dam/initData.java
+++ DojoNavalManuel/src/main/java/com/salesianostriana/dam/initData.java
@@ -26,14 +26,14 @@ public class initData {
 	
 	@PostConstruct
 	public void init () {
-		Alumno a =new Alumno ("Manuel","Naval","Rodríguez","navalrod@gmail.com","20072517A","651666999",LocalDate.of(1997, 04, 02),"Cádiz",11550, "Chipiona","Avd/Almería 74","img",  LocalDate.now(), "Si", "Marrón-Negro","Alergia estacional");
-		Alumno b =new Alumno ("Sara","Clarke","Swuch","clarkswuch@gmail.com","00055521J","000555444",LocalDate.of(1999, 05, 19),"Cádiz",12550, "Jerez","Calle fino","img",  LocalDate.now(), "Si", "Azul","");
-		Alumno c =new Alumno ("Antonio","Peinado","Rodríguez","navalrod@gmail.com","01247857H","651666999",LocalDate.of(1998, 05, 19),"Cádiz",11550, "Chipiona","calle girasol,20","img",  null, "No", "Blanco","Asma");
-		Alumno d =new Alumno ("Marina","Figueroa","Flores","navalrod@gmail.com","57489624G","651666999",LocalDate.of(1994, 05, 19),"Cádiz",11550, "Chipiona","AVD/Rosaleda sn","img",  null, "No", "Marrón","");
-		Alumno e =new Alumno ("Daniel","Villanueva","Garcia","navalrod@gmail.com","3333333E","651666999",LocalDate.of(1988, 05, 19),"Cádiz",11550, "Chipiona","Avd/Miguel Cervantes, 52","img",  null, "Si", "Negro","");
-		Alumno f =new Alumno ("Lorena","Cespedes","Roman","navalrod@gmail.com","2222222D","651666999",LocalDate.of(1989, 05, 19),"Cádiz",11550, "Chipiona","Avd/Andalucía,15","img",  null, "Si", "Amarillo","");
-		Alumno g =new Alumno ("Sergio","Arques","Tubio","navalrod@gmail.com","1111111C","651666999",LocalDate.of(1996, 05, 19),"Cádiz",11550, "Chipiona","calle/ Castaño, 10","img",  null, "No", "Verde","");
-		Alumno h =new Alumno ("Elon","Musk","Ramirez","navalrod@gmail.com","0000000B","651666999",LocalDate.of(2004, 05, 19),"Cádiz",11550, "Chipiona","Avd/Neptuno, 7","img",  null, "No", "Blanco-Amarillo","");
+		Alumno a =new Alumno ("Manuel","Naval","Rodríguez","navalrod@gmail.com","20072517A","651666999",LocalDate.of(1997, 04, 02),"Cádiz",11550, "Chipiona","Avd/Almería 74",null,  LocalDate.now(), "Si", "Marrón-Negro","Alergia estacional");
+		Alumno b =new Alumno ("Sara","Clarke","Swuch","clarkswuch@gmail.com","00055521J","000555444",LocalDate.of(1999, 05, 19),"Cádiz",12550, "Jerez","Calle fino",null,  LocalDate.now(), "Si", "Azul","");
+		Alumno c =new Alumno ("Antonio","Peinado","Rodríguez","navalrod@gmail.com","01247857H","651666999",LocalDate.of(1998, 05, 19),"Cádiz",11550, "Chipiona","calle girasol,20",null,  null, "No", "Blanco","Asma");
+		Alumno d =new Alumno ("Marina","Figueroa","Flores","navalrod@gmail.com","57489624G","651666999",LocalDate.of(1994, 05, 19),"Cádiz",11550, "Chipiona","AVD/Rosaleda sn",null,  null, "No", "Marrón","");
+		Alumno e =new Alumno ("Daniel","Villanueva","Garcia","navalrod@gmail.com","3333333E","651666999",LocalDate.of(1988, 05, 19),"Cádiz",11550, "Chipiona","Avd/Miguel Cervantes, 52",null,  null, "Si", "Negro","");
+		Alumno f =new Alumno ("Lorena","Cespedes","Roman","navalrod@gmail.com","2222222D","651666999",LocalDate.of(1989, 05, 19),"Cádiz",11550, "Chipiona","Avd/Andalucía,15",null,  null, "Si", "Amarillo","");
+		Alumno g =new Alumno ("Sergio","Arques","Tubio","navalrod@gmail.com","1111111C","651666999",LocalDate.of(1996, 05, 19),"Cádiz",11550, "Chipiona","calle/ Castaño, 10",null,  null, "No", "Verde","");
+		Alumno h =new Alumno ("Elon","Musk","Ramirez","navalrod@gmail.com","0000000B","651666999",LocalDate.of(2004, 05, 19),"Cádiz",11550, "Chipiona","Avd/Neptuno, 7",null,  null, "No", "Blanco-Amarillo","");
 		
 		
 		alumnoServicio.save(a);
